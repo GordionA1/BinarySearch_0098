143d9483e7ed4dad90d072d2912261bf9a5824d8
GordionA1 <farisazka2506@gmail.com>
2026-04-16 17:14:57 +0700

    menambahkan pengecekan kondisi

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -9,8 +9,11 @@ void input ()
 {
     while (true)
     {
-        cout << "Masukkan panjang elemen array: ";
+        cout << "Masukkan panjang elemen array (maksimal 10): ";
         cin >> nPanjang;
-   
+        if (nPanjang <= 10)
+            break;
+        else
+            cout << "\n[!]Panjang elemen array tidak boleh lebih dari 10.coba lagi!\n";
     }
 }
