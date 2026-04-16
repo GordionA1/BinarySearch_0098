799eb6ff61b3013f954ca801b07f40dad7d635a3
GordionA1 <farisazka2506@gmail.com>
2026-04-16 17:19:52 +0700

    menambahkan teks untuk input dan menambahkan pengecekan kondsi

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -11,9 +11,25 @@ void input ()
     {
         cout << "Masukkan panjang elemen array (maksimal 10): ";
         cin >> nPanjang;
+
         if (nPanjang <= 10)
+        {
             break;
+        }
         else
+        {
             cout << "\n[!]Panjang elemen array tidak boleh lebih dari 10.coba lagi!\n";
+        }
     }
+
+    cout << "\n===================================\n";
+    cout << "      Masukkan elemen array          \n";
+    cout << "\n===================================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> elemen[i];
+    }
+
 }
